24cfd2fe74c2852bc1411191b3506fe2948fd988
Huda Joad <hjoad@andrew.cmu.edu>
2023-10-05 12:17:28 +0300

    query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -25,7 +25,7 @@ export default function QueryProcessor(query: string): string | number {
       const largestNumber = Math.max(...intNumbers);
 
       // Return the largest number as a number
-      return largestNumber;
+      return largestNumber.toString();
     }
   }
 
@@ -43,7 +43,7 @@ export default function QueryProcessor(query: string): string | number {
       const result = x + y;
 
       // Return the result as a number
-      return result;
+      return result.toString();
     }
   }
 
