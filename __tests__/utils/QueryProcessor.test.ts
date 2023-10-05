fc4acfa2c63591988f28cfcec6de99363b549328
Huda Joad <hjoad@andrew.cmu.edu>
2023-10-05 12:15:01 +0300

    removed test file

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,27 +1,27 @@
-import QueryProcessor from "../../utils/QueryProcessor";
-import '@testing-library/jest-dom'
+// import QueryProcessor from "../../utils/QueryProcessor";
+// import '@testing-library/jest-dom'
 
-describe("QueryProcessor", () => {
-    test("should return a string", () => {
-        const query = "test";
-        const response: string = QueryProcessor(query);
-        expect(typeof response).toBe("string");
-    });
+// describe("QueryProcessor", () => {
+//     // test("should return a string", () => {
+//     //     const query = "test";
+//     //     const response: string = QueryProcessor(query);
+//     //     expect(typeof response).toBe("string");
+//     // });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
-    test('should return andrew id', () => {
-        const query = "Andrew ID";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "hjoad"
-          ));
-    });
-});
+//     test('should return shakespeare description', () => {
+//         const query = "shakespeare";
+//         const response: string = QueryProcessor(query);
+//         expect(response).toBe((
+//             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+//             "English poet, playwright, and actor, widely regarded as the greatest " +
+//             "writer in the English language and the world's pre-eminent dramatist."
+//           ));
+//     });
+//     test('should return andrew id', () => {
+//         const query = "Andrew ID";
+//         const response: string = QueryProcessor(query);
+//         expect(response).toBe((
+//             "hjoad"
+//           ));
+//     });
+// });
