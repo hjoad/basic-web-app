657f91eb67bade4b5941791dd3448b9537c045d7
Huda Joad <hjoad@andrew.cmu.edu>
2023-10-05 12:04:39 +0300

    query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,6 +16,20 @@ export default function QueryProcessor(query: string): string {
       "hjoad"
     );
   }
+  if (query.includes("Which of the following numbers is the largest:")) {
+    // Extract numbers from the query
+    const numbersRegex = /\d+/g;
+    const numbers = query.match(numbersRegex);
+
+    if (numbers && numbers.length >= 2) {
+      // Convert extracted numbers to an array of integers
+      const intNumbers = numbers.map((num) => parseInt(num, 10));
+
+      // Find the largest number
+      const largestNumber = Math.max(...intNumbers);
+      return largestNumber.toString();
+    }
+  }
 
   return "";
 }
