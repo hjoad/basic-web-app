0fda14a7169e76d75f0a0cd1b9130626e7ad0dc8
Huda Joad <hjoad@andrew.cmu.edu>
2023-10-05 11:50:40 +0300

    added name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,6 +11,11 @@ export default function QueryProcessor(query: string): string {
       "hjoad"
     );
   }
+  if (query.toLowerCase().includes("name")) {
+    return (
+      "huda"
+    );
+  }
 
   return "";
 }
